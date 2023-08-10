dc7fb0659e79b791e8d2fab0b55b854360250349
Phil Brown <pdbrown.git@gmail.com>
2023-08-09 18:54:20 -0700

    Better wipe.sh

--- wipe.sh
+++ wipe.sh
@@ -1,20 +1,29 @@
 #!/bin/bash
+# wipe.sh USERNAME
 set -e -o pipefail
-cd $(dirname $0)
-if [ -z $HOME ]; then
-  echo "HOME needs to be set"
+
+if ! [ "$1" ]; then
+  echo "Usage: $0 USERNAME"
   exit 1
 fi
-config_dir="$HOME/.config"
-if [ -n "$XDG_CONFIG_HOME" ]; then
-  config_dir="$XDG_CONFIG_HOME"
-fi
-data_dir="$HOME/.local/share"
-set -u
-set +e
-set -x
 
-# Restore permissions to containerd snapshot dirs
-find "$data_dir/usernetes" -type d -perm 000 2> /dev/null | xargs --no-run-if-empty chmod u+rwx
-# And delete
-rm -rf "$config_dir/usernetes" "$data_dir/usernetes"
+USER_HOME=$(bash -c "cd ~$(printf %q "$1") && pwd")
+
+config_dir="$USER_HOME/.config"
+data_dir="$USER_HOME/.local/share"
+
+echo About to run
+echo rm -rf \"$config_dir/usernetes\" \"$data_dir/usernetes\"
+
+function read_char() {
+  local prompt=$1
+  read -r -p "$prompt" -n 1 reply
+  echo ""
+}
+
+read_char "Continue? [yN] "
+case "$reply" in
+  y|Y)
+    sudo rm -rf "$config_dir/usernetes" "$data_dir/usernetes"
+    ;;
+esac
